9c9951a9f4f784d1bf8fda63b40c1016c5c12d56
Philip Sodervall <psodervall@twilio.com>
2017-12-13 11:02:56 +0100

    day 4b

--- day4/day4.go
+++ day4/day4.go
@@ -3,27 +3,63 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
+	"reflect"
+	"sort"
 	"strings"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	passphrases := parseFile("day4	.txt")
+	data := parseFile("day4.txt")
+	part1(data)
+	part2(data)
+}
+
+func part2(passphrases [][]string) {
+	var validPassphrases int
+	for _, row := range passphrases {
+		var valid = true
+		used := make(map[string]bool)
+		for _, word := range row {
+			for k := range used {
+				keySplit := strings.Split(k, "")
+				sort.Strings(keySplit)
+				wordSplit := strings.Split(word, "")
+				sort.Strings(wordSplit)
+				if reflect.DeepEqual(keySplit, wordSplit) {
+					valid = false
+					break
+				}
+			}
+			if valid {
+				used[word] = true
+			} else {
+				break
+			}
+		}
+		if valid {
+			validPassphrases++
+		}
+	}
+	fmt.Println(validPassphrases)
+}
+func part1(passphrases [][]string) {
 	var validPassphrases int
 	var unique bool
 	var tmpSlice []string
 	for _, passphrase := range passphrases {
 		unique = true
-		fmt.Println("++++++++++++++++++++")
 		for i, word := range passphrase {
 			//fmt.Print("passphrase:")
 			//fmt.Println(word)
 			tmpSlice = removdIndex(passphrase, i)
 			if findWord(tmpSlice, word) == false {
-				fmt.Println("FOUND MATCH FOR: ")
-				fmt.Print(word)
-				fmt.Println(tmpSlice)
 				unique = false
 			}
 		}
@@ -51,20 +87,16 @@ func findWord(slice []string, word string) bool {
 	return true
 }
 func parseFile(filePath string) [][]string {
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal(err)
+	f, err := os.Open(filePath)
+	check(err)
+	defer f.Close()
+
+	// Parse file
+	data := make([][]string, 0)
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		row := strings.Fields(s.Text())
+		data = append(data, row)
 	}
-	defer file.Close()
-	var myslice [][]string
-	var slice []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		slice = strings.SplitAfter(scanner.Text(), " ")
-		myslice = append(myslice, slice)
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	return myslice
+	return data
 }
